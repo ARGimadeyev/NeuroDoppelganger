1bbb1987545484353adc049c364fa69518ab79fd
Guess Who <ARGimadeyev@stud.kpfu.ru>
2024-12-16 15:28:42 +0300

    create tg bot

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,29 @@
 from config import TOKEN
 
-g = 1
+import logging, asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+# from aiogram.enums.parse_mode import ParseMode
+# from aiogram.client.default import DefaultBotProperties
+# from aiogram.types import InlineKeyboardButton, FSInputFile, \
+#     InputMediaPhoto
+# from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+# from aiogram import F
+# from typing import Optional
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.reply("Привет, ты запустил бота")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
