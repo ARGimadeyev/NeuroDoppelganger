9e33fca3f6a57bce8ae5b82e4d8221e4b78a648d
Guess Who <ARGimadeyev@stud.kpfu.ru>
2024-12-18 11:13:47 +0300

    testiki

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,11 @@
 from collections import deque
 from urllib.parse import ParseResult
 from aiogram.enums import ParseMode, ContentType
-from config import TOKEN
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import FSInputFile, InputMediaPhoto, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from config import TOKEN, dialogsHistory
 
 import logging, asyncio
 from aiogram import Bot, Dispatcher, types
@@ -9,12 +13,12 @@ from aiogram.filters.command import Command
 
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
-# from aiogram.types import InlineKeyboardButton, FSInputFile, \
-#     InputMediaPhoto
-# from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, FSInputFile, \
+    InputMediaPhoto
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
-# from typing import Optional
+from typing import Optional
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
@@ -37,12 +41,12 @@ async def forGPT(m: types.Message):
         send = f"ответил(а) {m.reply_to_message.from_user.full_name}"
     if m.content_type == ContentType.TEXT:
         return m.text + f" {send} {m.from_user.full_name}"
-    if m.text == None:
+    if m.caption == "":
         ans = f"{m.from_user.full_name} отправил(а) {m.content_type}"
         if m.content_type == ContentType.STICKER:
             ans += f"({m.sticker.emoji})"
         return ans
-    ans = f'"{m.text}", {send} {m.from_user.full_name}'
+    ans = f'"{m.caption}", {send} {m.from_user.full_name}'
     if m.content_type != ContentType.TEXT:
         ans += f" и {send} {m.content_type}"
         if m.content_type == ContentType.STICKER:
@@ -52,7 +56,52 @@ async def forGPT(m: types.Message):
 
 @dp.message(Command('start'))
 async def start(message: types.Message):
-    await message.reply(f"Привет, {message.from_user.full_name}!")
+    await message.reply(
+        f"Привет, {message.from_user.full_name}!\nЧтобы создать своего нейродвойника для чата напишите /newbot")
+
+
+ImportHistory = []
+user_data = dict()
+
+
+class NumbersCallbackFactory(CallbackData, prefix="history"):
+    action: str
+    value: Optional[int] = None
+
+
+async def get_keyboard(indx: int):
+    keyboard = InlineKeyboardBuilder()
+    col = 1
+    keyboard.button(text="Назад", callback_data=NumbersCallbackFactory(action='history_back', value=-1))
+    if indx < 3:
+        col += 1
+        keyboard.button(text="Дальше", callback_data=NumbersCallbackFactory(action='history_next', value=1))
+    keyboard.adjust(col)
+    return keyboard.as_markup()
+
+
+async def UpdateReplic(message: types.Message, indx: int):
+    await message.edit_media(InputMediaPhoto(media=ImportHistory[indx], caption=dialogsHistory[indx]),
+                             reply_markup=await get_keyboard(indx))
+
+
+@dp.message(Command('newbot'))
+async def newbot(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer_photo(photo=ImportHistory[0], caption=dialogsHistory[0],reply_markup=await get_keyboard(0))
+
+
+@dp.callback_query(NumbersCallbackFactory.filter())
+async def history(call: types.CallbackQuery, callback_data: NumbersCallbackFactory):
+    if call.from_user.id not in user_data:
+        user_data[call.from_user.id] = 0
+    user_data[call.from_user.id] += callback_data.value
+    if user_data[call.from_user.id] < 0:
+        user_data[call.from_user.id] = 0
+        await call.message.delete()
+        await newbot(call.message)
+    else:
+        await UpdateReplic(call.message, user_data[call.from_user.id])
 
 
 @dp.message(Command('top'))
@@ -77,5 +126,11 @@ async def main():
     await dp.start_polling(bot)
 
 
+def load():
+    for i in range(4):
+        ImportHistory.append(FSInputFile(f"ImportHistory/_{i}.png"))
+
+
 if __name__ == '__main__':
+    load()
     asyncio.run(main())
