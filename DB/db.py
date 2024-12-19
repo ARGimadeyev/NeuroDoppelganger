48b224b1778395b82b87dcdcd5a979321c315dee
Marsel <barsoff_21@mail.ru>
2024-12-19 12:47:55 +0300

    commit for db

---
+++ DB/db.py
@@ -0,0 +1,57 @@
+import json
+import psycopg2
+import asyncio
+import logging
+from collections import deque
+from email.message import Message
+from typing import Optional
+
+from aiogram import Bot, Dispatcher, types
+from aiogram import F
+from aiogram.enums import ContentType
+from aiogram.filters.callback_data import CallbackData
+from aiogram.filters.command import Command
+from aiogram.types import FSInputFile, \
+    InputMediaPhoto
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+conn = psycopg2.connect(
+
+)
+
+with open('result.json', 'r', encoding='utf-8') as f:
+    new_chat = json.load(f)
+
+cur = conn.cursor()
+
+def add_mess(group_id, messages):
+    for mes in messages:
+        if mes['type'] == 'service': continue
+        is_photo = mes.get("photo", "-")
+        if is_photo != '-':
+            b = ""
+            for s in mes['text_entities']:
+                b += s['text']
+            b = b.strip()
+            if len(b) == 0: continue
+            cur.execute(f"INSERT INTO i{group_id} VALUES ('{mes['from_id']}', 'photo', '{b}', '{mes['date']}')")
+            continue
+        b = ""
+        for s in mes['text_entities']:
+            b += s['text']
+        b = b.strip()
+        if len(b) == 0: continue
+        cur.execute(f"INSERT INTO i{group_id} VALUES ('{mes['from_id']}', 'text', '{b}', '{mes['date']}')")
+        if mes['text'] == '' and mes['media_type'] == 'sticker':
+            (cur.execute(
+                f"INSERT INTO i{group_id} VALUES ('{mes['from_id']}', '{mes['media_type']}', '{mes['sticker_emoji']}', '{mes['date']}')"))
+        elif mes.get("media_type", '-') != '-':
+            b = mes['text'].strip()
+            if len(b) == 0: continue
+            cur.execute(
+                f"INSERT INTO i{group_id} VALUES ('{mes['from_id']}', '{mes['media_type']}', '{b}', '{mes['date']}')")
+
+
+add_mess(str(new_chat['id']), new_chat['messages'])
+conn.commit()
+
